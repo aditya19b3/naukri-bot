3b2df1d89eadbd2b23c55738fd472e7ee7359ba0
aditya19b3 <adityabaghel125@gmail.com>
2026-07-23 04:20:26 +0530

    fix: add chrome compatibility arguments for Docker headless environment

--- utils.py
+++ utils.py
@@ -69,6 +69,9 @@ def build_driver(headless: bool = False, chromedriver_path: str = "") -> webdriv
     options = Options()
     if headless:
         options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     options.add_argument("--start-maximized")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--disable-notifications")
